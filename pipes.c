33e9e924446d1b341bc1cf9583565d511da8b23e
KenLi8504 <kli22@stuy.edu>
2021-12-08 22:40:14 -0500

    Completed assignment for one time, need to complete the infinite loop now

--- pipes.c
+++ pipes.c
@@ -10,29 +10,69 @@
 int main(){
   int parent2child[2];
   int child2parent[2];
+
   char input [10000];
   char output[10000];
+  char intermediate[10000];
   pipe(parent2child);
   pipe(child2parent);
   int f = fork();
-  while(1){
-    if (f != 0){
-      close(child2parent[WRITE]);
-      close(parent2child[READ]);
-      printf("Enter a string: ");
-      fgets(input,10000,stdin);
-      //printf("The string was %s",input);
-      write(parent2child[WRITE],input,strlen(input));
-      read(child2parent[READ],output,strlen(output));
-      printf("The output was %s",output);
+
+  //child
+  if (f == 0){
+    wait(NULL);
+    //strcpy(output,"Received!\n");
+    close(child2parent[READ]);
+    close(parent2child[WRITE]);
+    read(parent2child[READ],input,10000);
+    strcpy(intermediate,input);
+    //printf("The output here was %s\n",intermediate);
+    int counter = 0;
+    for (int i = 0; intermediate[i]!='\0'; i++) {
+      //printf("This char is %c\n",intermediate[i]);
+      //printf("The counter is %d",counter);
+      if (counter == 0){
+        if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+          intermediate[i] = intermediate[i] - 32;
+          counter = counter + 1;
+          //printf("OPTION 1");
+        }
+        else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+          counter = counter + 1;
+          //printf("OPTION 2");
+        }
+      }
+      else if (counter == 1){
+        if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+          counter = counter - 1;
+          //printf("OPTION 3");
+        }
+        else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+          intermediate[i] = intermediate[i] + 32;
+          counter = counter - 1;
+          //printf("OPTION 4");
+        }
+      }
+    //printf("The new string is %s",intermediate);
     }
-    else{
-      strcpy(output,"Received!\n");
-      close(child2parent[READ]);
-      close(parent2child[WRITE]);
-      read(parent2child[READ],input,strlen(input));
-      write(child2parent[WRITE],output,strlen(output));
-    }
+    strcpy(output,intermediate);
+    // printf("The length of the input is %ld\n",strlen(input));
+    //printf("The output here was %s\n",intermediate);
+    write(child2parent[WRITE],output,10000);
+  }
+
+  //parent
+  else{
+    close(child2parent[WRITE]);
+    close(parent2child[READ]);
+    printf("Enter a string: ");
+    fgets(input,10000,stdin);
+    char input_length_adjusted [strlen(input)];
+    strcpy(input_length_adjusted,input);
+    // //printf("The string was %s",input);
+    write(parent2child[WRITE],input_length_adjusted,10000);
+    read(child2parent[READ],output,strlen(input));
+    printf("The output from the child was %s\n",output);
   }
   return 0;
 }
