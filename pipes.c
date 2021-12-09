b672a54187d9e29e3751bcf029a7986c5291aa35
KenLi8504 <kli22@stuy.edu>
2021-12-08 22:50:32 -0500

    Finished the infinite loop, final commit

--- pipes.c
+++ pipes.c
@@ -19,60 +19,61 @@ int main(){
   int f = fork();
 
   //child
-  if (f == 0){
-    wait(NULL);
-    //strcpy(output,"Received!\n");
-    close(child2parent[READ]);
-    close(parent2child[WRITE]);
-    read(parent2child[READ],input,10000);
-    strcpy(intermediate,input);
-    //printf("The output here was %s\n",intermediate);
-    int counter = 0;
-    for (int i = 0; intermediate[i]!='\0'; i++) {
-      //printf("This char is %c\n",intermediate[i]);
-      //printf("The counter is %d",counter);
-      if (counter == 0){
-        if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-          intermediate[i] = intermediate[i] - 32;
-          counter = counter + 1;
-          //printf("OPTION 1");
+  while (1){
+    if (f == 0){
+      close(child2parent[READ]);
+      close(parent2child[WRITE]);
+      read(parent2child[READ],input,10000);
+      strcpy(intermediate,input);
+      //printf("The child here received here was %s\n",input);
+      //printf("The output here was %s\n",intermediate);
+      int counter = 0;
+      for (int i = 0; intermediate[i]!='\0'; i++) {
+        //printf("This char is %c\n",intermediate[i]);
+        //printf("The counter is %d",counter);
+        if (counter == 0){
+          if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+            intermediate[i] = intermediate[i] - 32;
+            counter = counter + 1;
+            //printf("OPTION 1");
+          }
+          else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+            counter = counter + 1;
+            //printf("OPTION 2");
+          }
         }
-        else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-          counter = counter + 1;
-          //printf("OPTION 2");
+        else if (counter == 1){
+          if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+            counter = counter - 1;
+            //printf("OPTION 3");
+          }
+          else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+            intermediate[i] = intermediate[i] + 32;
+            counter = counter - 1;
+            //printf("OPTION 4");
+          }
         }
+      //printf("The new string is %s",intermediate);
       }
-      else if (counter == 1){
-        if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-          counter = counter - 1;
-          //printf("OPTION 3");
-        }
-        else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-          intermediate[i] = intermediate[i] + 32;
-          counter = counter - 1;
-          //printf("OPTION 4");
-        }
-      }
-    //printf("The new string is %s",intermediate);
+      strcpy(output,intermediate);
+      // printf("The length of the input is %ld\n",strlen(input));
+      //printf("The output here was %s\n",intermediate);
+      write(child2parent[WRITE],output,10000);
     }
-    strcpy(output,intermediate);
-    // printf("The length of the input is %ld\n",strlen(input));
-    //printf("The output here was %s\n",intermediate);
-    write(child2parent[WRITE],output,10000);
-  }
 
-  //parent
-  else{
-    close(child2parent[WRITE]);
-    close(parent2child[READ]);
-    printf("Enter a string: ");
-    fgets(input,10000,stdin);
-    char input_length_adjusted [strlen(input)];
-    strcpy(input_length_adjusted,input);
-    // //printf("The string was %s",input);
-    write(parent2child[WRITE],input_length_adjusted,10000);
-    read(child2parent[READ],output,strlen(input));
-    printf("The output from the child was %s\n",output);
+    //parent
+    else{
+      close(child2parent[WRITE]);
+      close(parent2child[READ]);
+      printf("Enter a string: ");
+      fgets(input,10000,stdin);
+      char input_length_adjusted [strlen(input)];
+      strcpy(input_length_adjusted,input);
+      // //printf("The string was %s",input);
+      write(parent2child[WRITE],input_length_adjusted,10000);
+      read(child2parent[READ],output,10000);
+      printf("The output from the child was %s\n",output);
+    }
   }
   return 0;
 }
